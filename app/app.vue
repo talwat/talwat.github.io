084eb973ed561642238df91aeee61fb401340205
talwat <83217276+talwat@users.noreply.github.com>
2025-11-02 20:44:23 +0100

    feat: add icon

--- app/app.vue
+++ app/app.vue
@@ -1,19 +1,25 @@
 <script lang="ts" setup>
-  useSeoMeta({
-    title: "talwat's personal website",
-    ogTitle: "talwat's personal website",
-    description: 'Developer portfolio, including several different projects as well as contact information and an about me.',
-    ogDescription: 'Developer portfolio, including several different projects as well as contact information and an about me.',
-  })
+useSeoMeta({
+  title: "talwat's personal website",
+  ogTitle: "talwat's personal website",
+  description: 'Developer portfolio, including several different projects as well as contact information and an about me.',
+  ogDescription: 'Developer portfolio, including several different projects as well as contact information and an about me.',
+})
 
-  useHead({
-    meta: [
-      { name: "viewport", content: "width=device-width, initial-scale=1, viewport-fit=cover"},
-    ],
-    htmlAttrs: {
-      lang: "en"
-    }
-  })
+useHead({
+  meta: [
+    { name: "viewport", content: "width=device-width, initial-scale=1, viewport-fit=cover" },
+  ],
+  htmlAttrs: {
+    lang: "en"
+  },
+  link: [
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    { rel: 'manifest', href: '/site.webmanifest' }
+  ]
+})
 </script>
 
 <template>
